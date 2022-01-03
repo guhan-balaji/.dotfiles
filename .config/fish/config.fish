010759f4d49bc595ea824bb3bbb8c76d7da25b94
guhan-balaji <guhanbalaji7@gmail.com>
2022-01-03 16:50:50 +0530

    Add doom emacs config.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -165,7 +165,7 @@ alias jctl="journalctl -p 3 -xb"
 alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
 
 # Git alias for tracking dotfiles
-alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 ## Run paleofetch if session is interactive
 #if status --is-interactive
